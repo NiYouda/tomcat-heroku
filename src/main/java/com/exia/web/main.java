1c1fcaf72ee6b40b10581530d49ea708a6f4cba6
Niyouda <nicolas.judet@gmail.com>
2015-07-21 16:23:11 +0200

    commit

---
+++ src/main/java/com/exia/web/main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.exia.web;
+
+/**
+ *
+ * @author judet
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws Exception {
+ 
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();;
+ 
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+ 
+        tomcat.setPort(Integer.valueOf(webPort));
+ 
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+ 
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
